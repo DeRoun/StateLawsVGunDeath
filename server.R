4e626e8a86a65f9beac1adf4e453ffafd82b8c26
deroun <deroun@uw.edu>
2018-03-03 16:50:21 -0800

    All state leaflet output added

--- server.R
+++ server.R
@@ -1,6 +1,76 @@
-library("shiny")
+suppressPackageStartupMessages(library("shiny"))
+suppressPackageStartupMessages(library("leaflet"))
+suppressPackageStartupMessages(library("dplyr"))
+suppressPackageStartupMessages(library("sp"))
+suppressPackageStartupMessages(library("ggplot2"))
+suppressPackageStartupMessages(library("rgdal"))
+suppressPackageStartupMessages(library("htmltools"))
+suppressPackageStartupMessages(library("mapview"))
+suppressPackageStartupMessages(library("htmlwidgets"))
+suppressPackageStartupMessages(library("albersusa"))
 
 server <- function(input, output){
+  
+  # Map data from albersusa package for USA map with compatable alaska and hawaii projections
+
+  spdf <- rmapshaper::ms_simplify(usa_composite())
+  
+  output$map.As <- renderLeaflet({
+    
+    # Filters data frame bases on All State Control widgets
+    
+    remove <- c("All States")
+    allStates <- filter(final.df, Year == input$yearChoice.As) %>%
+      filter(!State %in% remove)
+    allStates <- allStates[match(spdf$name, allStates$State),]
+    
+    # Sets bins for fill color, then creates colorbin with set of colors equal to
+    # the bin in relation to allStates.df$Rate.
+    
+    bins <- seq(2, 35, by = 3)
+    pal <- colorBin(c("#fff5f0", "#fee0d2", "#fcbba1", "#fc9272", "#fb6a4a", 
+                      "#ef3b2c", "#cb181d", "#a50f15", "#67000d"),
+                    domain = allStates$Rate, bins = bins)
+    
+    # Creates projection for hawaii and Alaska on all state map
+    epsg2163 <- leafletCRS(
+      crsClass = "L.Proj.CRS",
+      code = "EPSG:2163",
+      proj4def = "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs",
+      resolutions = 2^(16:7))
+    
+    # Creates label variable to be used on map
+    
+    labels <- paste("<p>", allStates$State, "</p>",
+                    "<p>", "Gun Death Rate: ", allStates$Rate, "</p>",
+                    "<p>", "Total Gun Laws: ", allStates$LawTotal, "</p>",
+                    sep="")
+    # Creates Leaflet Plot to be output
+    
+    leaflet(spdf, options = leafletOptions(crs = epsg2163)) %>%
+      setView(-96, 37.8, 4) %>%
+      addPolygons(fillColor = pal(allStates$Rate),
+                  weight = 1,
+                  smoothFactor = 0.5,
+                  color = "white",
+                  fillOpacity = 0.8,
+                  highlight = highlightOptions(
+                    weight = 5,
+                    color = "#666",
+                    dashArray = "",
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE),
+                  label = lapply(labels, HTML)) %>%
+      addLegend(pal = pal,
+                values = allStates$Rate,
+                opacity = 0.7,
+                title = "Rate per 100,000 People",
+                position = "bottomright")
+    
+  })
+  
+  
+  
   
 }
 
